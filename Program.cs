de2562cd37d0790a07c4b3462d6158b0d06b7ad3
Anton Dorofeyev <dorofeyev@gmail.com>
2017-12-04 12:08:33 +0200

    adding data models

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace net.c4d.mongodb.transactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration config =  new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            
+        }
+    }
+}
